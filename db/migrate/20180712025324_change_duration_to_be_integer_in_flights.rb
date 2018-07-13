10eef58774392175c0b980864f0b609c583ba329
Punnadit Trussat <punnadittr@gmail.com>
2018-07-13 16:01:39 +0700

    fixed integer for pg

--- db/migrate/20180712025324_change_duration_to_be_integer_in_flights.rb
+++ db/migrate/20180712025324_change_duration_to_be_integer_in_flights.rb
@@ -1,5 +1,5 @@
 class ChangeDurationToBeIntegerInFlights < ActiveRecord::Migration[5.2]
   def change
-    change_column :flights, :duration, 'integer USING CAST(column_name AS integer)'
+    change_column :flights, :duration, 'integer USING CAST(duration AS integer)'
   end
 end
